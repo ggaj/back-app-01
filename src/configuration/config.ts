e1d3873eb7309f874472a85cefd407b9ff1ad6f1
Gildo <junioripg@gmail.com>
2019-09-29 22:37:56 -0400

    tsc

--- src/configuration/config.ts
+++ src/configuration/config.ts
@@ -1,5 +1,5 @@
 export default {
-  port: process.env.PORT || 3333,
+  port: process.env.PORT || 3334,
   secretyKey: process.env.SECRETYKEY || 'f5ccc0f3-cbb6-4788-b4c8-403c22cd110f',
   publicRoutes: process.env.PUBLICROUTES || [
     'users/create',
